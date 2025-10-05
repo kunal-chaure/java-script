9a793415947ed2adc6a812dce2517eaf09b78b31
kunal-chaure <kunalchaure40@gmail.com>
2025-10-05 04:01:23 +0000

    container config done

--- .devcontainer/01_basics/variable.js
+++ .devcontainer/01_basics/variable.js
@@ -2,4 +2,5 @@ const account = 112233
 let a = "1122"
 var no ="kunal"
 // account="11" this is not allowed by node js as it is constant
-console.log(account)
+console.log(account);
+console.log("kunal is here ");
